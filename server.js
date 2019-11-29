c16ebccee4a5dc3c43249d96aadcde1c39720115
Brian Gates <brian.g.gates@gmail.com>
2019-11-29 15:06:52 -0800

    Cleaned up server

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const http = require('http')
 const express = require('express')
 const app = express()
 
-const hostname = '127.0.0.1'
 const port = 3000
 
 app.use(express.static('.'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Running: http://localhost:${port}`))
