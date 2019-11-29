e26a1821a6436ec1d57e36bf7b43746e1f8db177
Brian Gates <brian.g.gates@gmail.com>
2019-11-29 15:43:53 -0800

    added movement script, relates to #3

---
+++ src/Unity/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(
+            Input.GetAxis("Horizontal") * Time.deltaTime * speed, 
+            0f,
+            Input.GetAxis("Vertical") * Time.deltaTime * speed
+        );
+    }
+}
