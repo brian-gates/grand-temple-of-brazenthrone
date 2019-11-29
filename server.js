fd3597e1b6b4480cbb95b090eb72f274ce41eb17
Brian Gates <brian.g.gates@gmail.com>
2019-11-29 12:45:02 -0800

    move to root for gh-pages

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express()
 const hostname = '127.0.0.1'
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static('.'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
