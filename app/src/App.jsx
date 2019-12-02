82bb5e5042202c27b4347ebc9c379b90c5642f08
Brian Gates <brian.g.gates@gmail.com>
2019-12-01 22:49:18 -0800

    layout improvements

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -6,13 +6,18 @@ const unityContent = new UnityContent(
   "Build.json",
   "UnityLoader.js"
 );
+
 export const App = () => {
   
   return (
     <div className="App">
       <a className="github-fork-ribbon" href="https://github.com/brian-gates/grand-temple-of-brazenthrone" data-ribbon="Fork me on GitHub" title="Fork me on GitHub">Fork me on GitHub</a>
-      <h1>The Grand Temple of Brazenthrone</h1>
-      <Unity unityContent={unityContent} />
+      <div style={{ height: "100vh", width: "100vw", position: "absolute", top: 0, left: 0 }}>
+        <Unity unityContent={unityContent}  />
+      </div>
+      <div style={{ height: "100vh", width: "100vw", position: "absolute", top: 0, left: 0 }}>
+        <h1>The Grand Temple of Brazenthrone</h1>
+      </div>
     </div>
   );
 }
