ec7ea9de10f85cfe452a5b6ba164c4ba9618b2df
Brian Gates <brian.g.gates@gmail.com>
2019-12-01 18:23:03 -0800

    disable flight

--- src/Unity/Assets/Scripts/Movement.cs
+++ src/Unity/Assets/Scripts/Movement.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    CharacterController characterController;
+
     public float speed = 10f;
+    private Vector3 moveDirection = Vector3.zero;
+    public float gravity = 20.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(
-            Input.GetAxis("Horizontal") * Time.deltaTime * speed, 
-            0f,
-            Input.GetAxis("Vertical") * Time.deltaTime * speed
-        );
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * Input.GetAxis("Vertical");
+        Vector3 strafe = transform.TransformDirection(Vector3.right) * Input.GetAxis("Horizontal");
+        Vector3 moveDirection = (forward + strafe) * speed * Time.deltaTime;
+        moveDirection.y -= gravity * Time.deltaTime;
+        Debug.DrawRay(transform.position, forward*10, Color.blue);
+        Debug.DrawRay(transform.position, strafe*10, Color.red);
+        Debug.DrawRay(transform.position, moveDirection*10, Color.green);
+        characterController.Move(moveDirection);
     }
 }
